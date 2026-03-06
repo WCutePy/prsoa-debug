2a5304925483d2b06324010e246a4acd63d3896d
HappyLappy1 <86489014+HappyLappy1@users.noreply.github.com>
2026-03-05 23:05:06 -0500

    Create overlay_29.h

---
+++ headers/functions/overlay_29.h
@@ -0,0 +1,8 @@
+undefined4 FUN_overlay_29__0211c9e0(void);
+void FUN_overlay_29__0211c9e8(void);
+void FUN_overlay_29__0211c9ec(void);
+void FUN_overlay_29__0211cb54(void);
+void FUN_overlay_29__0211cc44(undefined4 param_1,undefined4 param_2,undefined4 param_3,undefined4 param_4);
+undefined4 FUN_overlay_29__0211cc90(undefined4 param_1);
+undefined4 FUN_overlay_29__0211ccac(void);
+undefined4 FUN_overlay_29__0211ccb4(undefined4 param_1);
