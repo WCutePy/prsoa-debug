a6bbf0996a382bab54e4514c8c229b91293be168
Parker Zhiss <86489014+HappyLappy1@users.noreply.github.com>
2026-03-25 23:30:01 -0400

    oops

--- headers/functions/overlay22.h
+++ headers/functions/overlay22.h
@@ -1,6 +1,7 @@
 #ifndef HEADERS_FUNCTIONS_OVERLAY22_H_
 #define HEADERS_FUNCTIONS_OVERLAY22_H_
 
+struct ov_22_unk_struct * Overlay22EntryPoint(struct ov_22_unk_struct *param_1);
 void ret_0211cd7c(void);
 void ret_0211d2d0(void);
 void ret_0211e158(void);
