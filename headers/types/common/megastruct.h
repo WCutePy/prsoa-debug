727887b5c4c1e83930965001c40d6d884a3ce99f
Parker Zhiss <86489014+HappyLappy1@users.noreply.github.com>
2026-03-28 16:03:24 -0400

    Fix Syntax

--- headers/types/common/megastruct.h
+++ headers/types/common/megastruct.h
@@ -7,7 +7,7 @@ struct mega_substruct_1 {
     undefined2 unk_field_0x8;
     undefined2 unk_field_0xa;
     undefined2 unk_field_0xc;
-    undefined2 unk_field_0xe; 
+    undefined2 unk_field_0xe;
 };
 ASSERT_SIZE(struct mega_substruct_1, 16);
 
@@ -27,9 +27,12 @@ struct megastruct {
     // Supposedly a set of caches. Unsure what type it uses, but they're 1024 bytes long.
     undefined1 unk_cashe_1[1024];
     undefined1 unk_cache_2[1024];
-    undefined1 unk_cache_3[1024]; // This one is initialized differently than the others. Not sue why.
-    void* unk_struct_size_0x4c;   // Seems related to 2D and 3D background (?) display (2D Engine A, B, and 3D engine)
-    void* unk_struct_size_0x4c_1; // Seems related to 2D and 3D background (?) display (2D Engine A, B, and 3D engine)
+    undefined1
+        unk_cache_3[1024];      // This one is initialized differently than the others. Not sue why.
+    void* unk_struct_size_0x4c; // Seems related to 2D and 3D background (?) display (2D Engine A,
+                                // B, and 3D engine)
+    void* unk_struct_size_0x4c_1; // Seems related to 2D and 3D background (?) display (2D Engine A,
+                                  // B, and 3D engine)
     void* unk_struct_size_0x80;
     void* unk_struct_size_0x80_1;
     // This is likely a table of substructs. Not a clue what they do though!
@@ -50,13 +53,14 @@ struct megastruct {
     undefined4 field3842_0x1468;
     undefined field3843_0x146c[32];
     void* unk_struct_size_0x1ac;
-    // A pointer to this is present in the mission_quest_footer. Looks to be related to the message given before a quest is accepted.
-    // Has ties to "data/message/quest/quest%03d_mes"
+    // A pointer to this is present in the mission_quest_footer. Looks to be related to the message
+    // given before a quest is accepted. Has ties to "data/message/quest/quest%03d_mes"
     void* unk_struct_size_0x158;
     void* unk_struct_size_0x44;
     void* unk_struct_size_0x40;
-    // A pointer to this is present in the mission_quest_footer. Might be related to quest script files "data/Script/quest/q%03d.fsb"
-    void* unk_struct_size_0x1881C; 
+    // A pointer to this is present in the mission_quest_footer. Might be related to quest script
+    // files "data/Script/quest/q%03d.fsb"
+    void* unk_struct_size_0x1881C;
     struct mission_quest_data* mission_quest_data_ptr;
     struct ranger_data_old* ranger_data_ptr;
     undefined4 field3882_0x14a8;
@@ -64,13 +68,15 @@ struct megastruct {
     void* unk_struct_size_0x14;
     struct settings_and_variables* settings_vars_ptr;
     void* unk_struct_size_0x28;
-    void* unk_struct_size_0x1c;    // Might be related to sound registers? See FUN_0200E2D4
-    void* unk_struct_size_0x1c_1;  // Might be related to sound registers? See FUN_0200E2D4
-    void* unk_struct_size_0xc7c;   // Seems related to 2D display (2D Engine A and B), See FUN_200E4A8.
-    void* unk_struct_size_0xc7c_1; // Seems related to 2D display (2D Engine A and B), See FUN_200E4A8.
-    void* field_data_bin_ptr; // Likely a pointer to a decompressed copy of fielddata.bin 
-    void* field3892_0x14d0[32]; // These appear to be functions for the below param_0s
-    int field3893_0x1550[32]; // These appear to be param_0 for the above functions
+    void* unk_struct_size_0x1c;   // Might be related to sound registers? See FUN_0200E2D4
+    void* unk_struct_size_0x1c_1; // Might be related to sound registers? See FUN_0200E2D4
+    void*
+        unk_struct_size_0xc7c; // Seems related to 2D display (2D Engine A and B), See FUN_200E4A8.
+    void* unk_struct_size_0xc7c_1; // Seems related to 2D display (2D Engine A and B), See
+                                   // FUN_200E4A8.
+    void* field_data_bin_ptr;      // Likely a pointer to a decompressed copy of fielddata.bin
+    void* field3892_0x14d0[32];    // These appear to be functions for the below param_0s
+    int field3893_0x1550[32];      // These appear to be param_0 for the above functions
     undefined4 field4021_0x15d0;
     undefined4 field4022_0x15d4;
     void* unk_struct_size_0x58;
--- headers/types/common/options_menu.h
+++ headers/types/common/options_menu.h
@@ -4,25 +4,31 @@
 
 // All ov_06 functions that take parameters seem to take this as param_1.
 struct options_menu {
-    void *unk_fun_ptr;
-    undefined *unk_struct_size_0x44_ptr_1;
-    undefined *unk_struct_size_0x44_ptr_2;
-    undefined *text_speed_button_struct_ptr[4]; // When poked, the text speed buttons lose functionality
-    undefined *window_type_button_struct_ptr[8]; // When poked, the window buttons lose functionality
-    undefined *back_button_struct_ptr; // When poked, the back button stops working.
-    int icon_types_text_speed[3]; // Determines the which icons should be used for the text speed slots.
-    int icon_types_windows[8]; // Determines the which icons should be used for the window option slots.
+    void* unk_fun_ptr;
+    undefined* unk_struct_size_0x44_ptr_1;
+    undefined* unk_struct_size_0x44_ptr_2;
+    undefined*
+        text_speed_button_struct_ptr[4]; // When poked, the text speed buttons lose functionality
+    undefined*
+        window_type_button_struct_ptr[8]; // When poked, the window buttons lose functionality
+    undefined* back_button_struct_ptr;    // When poked, the back button stops working.
+    int icon_types_text_speed[3]; // Determines the which icons should be used for the text speed
+                                  // slots.
+    int icon_types_windows[8];    // Determines the which icons should be used for the window option
+                                  // slots.
     int cursor_icon_pos; // Determines which icon hovers over the most recently selected option.
-    struct megastruct *megastruct_ptr;
-    int screen_fadeout_timer; // If not zero, counts down the number of frames until the overlay should be exited.
+    struct megastruct* megastruct_ptr;
+    int screen_fadeout_timer; // If not zero, counts down the number of frames until the overlay
+                              // should be exited.
     undefined field11_0x78;
     undefined field12_0x79;
     undefined field13_0x7a;
     undefined field14_0x7b;
     int window_setting;
     int text_speed_setting;
-    undefined4 *unk_struct_size_0xc_ptr_1; // When poked, the entire menu freezes. Presumed to handle idle animations?
-    undefined *unk_struct_size_0xc_ptr_2; // When poked, the back button causes a freeze.
+    undefined4* unk_struct_size_0xc_ptr_1; // When poked, the entire menu freezes. Presumed to
+                                           // handle idle animations?
+    undefined* unk_struct_size_0xc_ptr_2;  // When poked, the back button causes a freeze.
 };
 
 ASSERT_SIZE(struct options_menu, 140);
