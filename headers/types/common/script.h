0755deebfa4cf74881a010e46d7dbcac2f58e2dd
Parker Zhiss <86489014+HappyLappy1@users.noreply.github.com>
2026-03-29 01:09:08 -0400

    Add Scripting Analysis to Pull Request
    I give up. Sorry chesyon, I'm putting it here.

---
+++ headers/types/common/script.h
@@ -0,0 +1,79 @@
+
+struct script_data {
+    char script_file_name[12];
+    undefined field1_0xc;
+    undefined field2_0xd;
+    undefined field3_0xe;
+    undefined field4_0xf;
+    undefined field5_0x10;
+    undefined field6_0x11;
+    undefined field7_0x12;
+    undefined field8_0x13;
+    undefined field9_0x14;
+    undefined field10_0x15;
+    undefined field11_0x16;
+    undefined field12_0x17;
+    undefined field13_0x18;
+    undefined field14_0x19;
+    undefined field15_0x1a;
+    undefined field16_0x1b;
+    undefined field17_0x1c;
+    undefined field18_0x1d;
+    undefined field19_0x1e;
+    undefined field20_0x1f;
+    undefined4 field21_0x20;
+    undefined4 field22_0x24;
+    void *unk_struct_0xb8; // Unknown struct of size 0xb8.
+    char subroutine_1_name[12]; // Not sure how subroutines 1 and 2 differ yet...
+    undefined field25_0x38[552];
+    int subroutine_1_is_active;
+    char subroutine_2_name[12];
+    undefined field28_0x270;
+    undefined field29_0x271;
+    undefined field30_0x272;
+    undefined field31_0x273;
+    int is_subroutine_2_active;
+    int script_index;
+};
+ASSERT_SIZE(struct script_data, 636);
+
+struct script_manager {
+    undefined4 *megastruct_field_0x14a8_ptr;
+    struct script_data *system_script_data_ptr;
+    struct script_data *area_script_data_ptr;
+    struct script_data *chapter_script_data_ptr;
+    struct script_data *quest_script_data_ptr;
+    undefined4 unk_riding_pokemon_data; // Changes when riding doduo, among other things?
+    undefined4 field6_0x18;
+    undefined field7_0x1c;
+    undefined field8_0x1d;
+    undefined field9_0x1e;
+    undefined field10_0x1f;
+    undefined field11_0x20;
+    undefined field12_0x21;
+    undefined field13_0x22;
+    undefined field14_0x23;
+    undefined field15_0x24;
+    undefined field16_0x25;
+    undefined field17_0x26;
+    undefined field18_0x27;
+    undefined field19_0x28;
+    undefined field20_0x29;
+    undefined field21_0x2a;
+    undefined field22_0x2b;
+    undefined field23_0x2c;
+    undefined field24_0x2d;
+    undefined field25_0x2e;
+    undefined field26_0x2f;
+    undefined field27_0x30;
+    undefined field28_0x31;
+    undefined field29_0x32;
+    undefined field30_0x33;
+    undefined field31_0x34;
+    undefined field32_0x35;
+    undefined field33_0x36;
+    undefined field34_0x37;
+    void *unk_struct_0x3c_ptr; // Points to 0x3C...
+    undefined field36_0x3c[100320];
+};
+ASSERT_SIZE(struct script_manager, 100380);
