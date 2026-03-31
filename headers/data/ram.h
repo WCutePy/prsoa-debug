e7e8c6477acd3c13ca009c8c5f4ffe0265cc1b8f
Parker Zhiss <86489014+HappyLappy1@users.noreply.github.com>
2026-03-30 22:09:59 -0400

    Let's try that again...

--- headers/data/ram.h
+++ headers/data/ram.h
@@ -8,7 +8,7 @@ extern struct mission_quest_data MISSION_QUEST_DATA;
 extern struct ranger_data RANGER_DATA;
 extern struct pokemon_data PARTY_POKEMON_TABLES[3][10];
 extern struct following_npc FOLLOWING_NPC_TABLE[2];
-extern int ARENA_GRAPHIC_ID;
+int ARENA_GRAPHIC_ID;
 extern struct settings_and_variables SETTINGS_AND_VARIABLES;
 extern void** SYSCALL_PAGE_PTRS[64];
 #endif
