c2e87d3c9d0e440860887359dfe1fa5b9066447f
Parker Zhiss <86489014+HappyLappy1@users.noreply.github.com>
2026-04-06 23:49:33 -0400

    Let's see how badly it explodes...

---
+++ headers/functions/arm9/itcm.h
@@ -0,0 +1,12 @@
+#ifndef HEADERS_FUNCTIONS_ARM9_ITCM_H_
+#define HEADERS_FUNCTIONS_ARM9_ITCM_H_
+void OSi_DoResetSystem(void);
+void OSi_DoBoot(void);
+void OSi_CpuClear32(undefined4 param_1,undefined4 *param_2,int32_t param_3);
+void OSi_ReloadRomData(void);
+void OSi_ReadCardRom32(uint32_t param_1,int32_t param_2,int32_t param_3);
+void MIi_DmaSetParams(int32_t param_1,undefined4 param_2,undefined4 param_3,undefined4 param_4);
+void MIi_DmaSetParams_wait(int32_t param_1,undefined4 param_2,undefined4 param_3,undefined4 param_4);
+undefined4 MIi_DmaSetParams_wait_src32(int32_t param_1,undefined4 param_2,undefined4 param_3,undefined4 param_4);
+undefined4 MIi_UncompressBackward(int32_t param_1);
+#endif
