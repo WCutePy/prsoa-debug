340cf1aba5ec4828b6a6fe2e818414c6e361d252
Parker Zhiss <86489014+HappyLappy1@users.noreply.github.com>
2026-04-15 23:11:02 -0400

    New Functions

--- headers/functions/overlay02.h
+++ headers/functions/overlay02.h
@@ -1,11 +1,14 @@
 #ifndef HEADERS_FUNCTIONS_OVERLAY02_H_
 #define HEADERS_FUNCTIONS_OVERLAY02_H_
 
-void ret_0211d124(void);
-void ret_0211d128(void);
-void ret_0211d12c(void);
-void ret_0211d130(void);
-void ret_0211d134(void);
-void ret_0211d138(void);
+void PostprocessBattleResults(struct ov_00_unk_struct *param_1);
+uint32_t ChooseReturnOverlayId(struct ov_00_unk_struct *param_1);
+void ret_0211D124(void);
+void ret_0211D128(void);
+void ret_0211D12C(void);
+void ret_0211D130(void);
+void ret_0211D134(void);
+void ret_0211D138(void);
+undefined4 ov_02_return_0x5(void);
 
 #endif
--- headers/functions/overlay29.h
+++ headers/functions/overlay29.h
@@ -2,5 +2,7 @@
 #define HEADERS_FUNCTIONS_OVERLAY29_H_
 
 void ret_0211c9e8(void);
+void InitGraphicsOv29(void);
+void PrintCartErrorMsg(void);
 
 #endif
