1722c8951cfd119a2d7033bb7fb856d47136ac55
Parker Zhiss <86489014+HappyLappy1@users.noreply.github.com>
2026-04-17 15:48:47 -0400

    Revert "New structs and fields"
    This reverts commit f2db3534dc3b3d2a30118eea1c4a007cc900fa78.

--- headers/types/common/script.h
+++ headers/types/common/script.h
@@ -1,19 +1,5 @@
 #ifndef HEADERS_TYPES_COMMON_SCRIPT_H_
 #define HEADERS_TYPES_COMMON_SCRIPT_H_
-
-#include "file.h"
-
-// Seems to be a decompressed .fsb script file. Maybe someone else has researched this topic? :P
-struct script_file {
-    int unk_pointer_0x0;
-    void *unk_ptr_0x4;
-    void *unk_ptr_0x8;
-    char *unk_ptr_0xC;
-    int zero;
-};
-ASSERT_SIZE(struct script_file, 20);
-
-
 struct script_data {
     char script_file_name[12];
     undefined field1_0xc;
@@ -36,9 +22,9 @@ struct script_data {
     undefined field18_0x1d;
     undefined field19_0x1e;
     undefined field20_0x1f;
-    struct script_file *script_file_ptr;
-    int script_file_offset;
-    struct file_wrapper *script_file_wrapper;
+    undefined4 field21_0x20;
+    undefined4 field22_0x24;
+    void* unk_struct_0xb8;      // Unknown struct of size 0xb8.
     char subroutine_1_name[12]; // Not sure how subroutines 1 and 2 differ yet...
     undefined field25_0x38[552];
     int subroutine_1_is_active;
@@ -53,14 +39,33 @@ struct script_data {
 ASSERT_SIZE(struct script_data, 636);
 
 struct script_manager {
-    undefined4 *megastruct_field_0x14a8_ptr;
-    struct script_data *system_script_data_ptr;
-    struct script_data *area_script_data_ptr;
-    struct script_data *chapter_script_data_ptr;
-    struct script_data *quest_script_data_ptr;
-    undefined4 unk_riding_pokemon_data;
+    undefined4* megastruct_field_0x14a8_ptr;
+    struct script_data* system_script_data_ptr;
+    struct script_data* area_script_data_ptr;
+    struct script_data* chapter_script_data_ptr;
+    struct script_data* quest_script_data_ptr;
+    undefined4 unk_riding_pokemon_data; // Changes when riding doduo, among other things?
     undefined4 field6_0x18;
-    struct script_file chx_script_file; // Seems to be a raw script_file struct. What makes chx### so special?
+    undefined field7_0x1c;
+    undefined field8_0x1d;
+    undefined field9_0x1e;
+    undefined field10_0x1f;
+    undefined field11_0x20;
+    undefined field12_0x21;
+    undefined field13_0x22;
+    undefined field14_0x23;
+    undefined field15_0x24;
+    undefined field16_0x25;
+    undefined field17_0x26;
+    undefined field18_0x27;
+    undefined field19_0x28;
+    undefined field20_0x29;
+    undefined field21_0x2a;
+    undefined field22_0x2b;
+    undefined field23_0x2c;
+    undefined field24_0x2d;
+    undefined field25_0x2e;
+    undefined field26_0x2f;
     undefined field27_0x30;
     undefined field28_0x31;
     undefined field29_0x32;
@@ -69,7 +74,7 @@ struct script_manager {
     undefined field32_0x35;
     undefined field33_0x36;
     undefined field34_0x37;
-    void *unk_struct_0x3c_ptr;
+    void* unk_struct_0x3c_ptr; // Points to 0x3C...
     undefined field36_0x3c[100320];
 };
 ASSERT_SIZE(struct script_manager, 100380);
